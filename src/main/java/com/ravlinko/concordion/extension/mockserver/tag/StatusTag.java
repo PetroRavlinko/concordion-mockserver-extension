c9096d97935790e8bb63db4675d5abd0ad22a9e3
Petro Ravlinko <Petro.Ravlinko@symphony-solutions.eu>
2017-03-17 16:05:51 +0200

    close #3 add response status code tag

---
+++ src/main/java/com/ravlinko/concordion/extension/mockserver/tag/StatusTag.java
@@ -0,0 +1,23 @@
+package com.ravlinko.concordion.extension.mockserver.tag;
+
+import org.concordion.api.CommandCall;
+import org.concordion.api.Element;
+import org.concordion.api.Evaluator;
+import org.concordion.api.ResultRecorder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class StatusTag extends MockServerTag {
+	public StatusTag() {
+		setName("statusCode");
+		setHttpName("code");
+	}
+
+	@Override
+	public void setUp(CommandCall commandCall, Evaluator evaluator, ResultRecorder resultRecorder) {
+		Element element = commandCall.getElement();
+		String value = element.getText();
+		ResponseTag.responseFromEvaluator(evaluator).withStatusCode(Integer.parseInt(value));
+	}
+
+}
