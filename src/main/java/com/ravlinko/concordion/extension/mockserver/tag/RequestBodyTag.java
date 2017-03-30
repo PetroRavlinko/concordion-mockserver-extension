5cd7f742c5c041528559496ef5d18d385a245845
Petro Ravlinko <Petro.Ravlinko@symphony-solutions.eu>
2017-03-30 16:15:37 +0300

    close #17 Retrieve and set expected request body from stored file

--- src/main/java/com/ravlinko/concordion/extension/mockserver/tag/RequestBodyTag.java
+++ src/main/java/com/ravlinko/concordion/extension/mockserver/tag/RequestBodyTag.java
@@ -4,11 +4,17 @@ package com.ravlinko.concordion.extension.mockserver.tag;
 import com.cedarsoftware.util.io.JsonWriter;
 import com.ravlinko.concordion.extension.mockserver.utils.JsonPrettyPrinter;
 
+import org.apache.commons.io.IOUtils;
 import org.concordion.api.CommandCall;
 import org.concordion.api.Element;
 import org.concordion.api.Evaluator;
 import org.concordion.api.ResultRecorder;
 import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 import static org.mockserver.model.StringBody.exact;
 
@@ -23,9 +29,17 @@ public class RequestBodyTag extends MockServerTag {
 	@Override
 	public void setUp(CommandCall commandCall, Evaluator evaluator, ResultRecorder resultRecorder) {
 		Element element = commandCall.getElement();
-		element.addStyleClass("json");
-
-		String body = new JsonPrettyPrinter().prettyPrint(element.getText());
+		String resource = element.getAttributeValue("resource");
+		String body = null;
+		if (resource != null) {
+			try {
+				body = IOUtils.toString(Files.newInputStream(Paths.get(ClassLoader.getSystemResource(resource).toURI())));
+			} catch (IOException | URISyntaxException e) {
+				e.printStackTrace();
+			}
+		} else {
+			body = new JsonPrettyPrinter().prettyPrint(element.getText());
+		}
 		element.appendText(body);
 		evaluator.setVariable(MOCK_REQUEST_BODY_VARIABLE, body);
 
