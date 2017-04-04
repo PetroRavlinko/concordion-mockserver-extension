a03076135442ba090032b76402f9e137946ba27b
Petro Ravlinko <Petro.Ravlinko@symphony-solutions.eu>
2017-04-04 12:33:55 +0300

    fix displaing content of files in report

--- src/main/java/com/ravlinko/concordion/extension/mockserver/tag/RequestBodyTag.java
+++ src/main/java/com/ravlinko/concordion/extension/mockserver/tag/RequestBodyTag.java
@@ -1,14 +1,16 @@
 package com.ravlinko.concordion.extension.mockserver.tag;
 
 
-import com.cedarsoftware.util.io.JsonWriter;
 import com.ravlinko.concordion.extension.mockserver.utils.JsonPrettyPrinter;
+import com.ravlinko.concordion.extension.mockserver.utils.XmlPrettyPrinter;
 
 import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.StringEscapeUtils;
 import org.concordion.api.CommandCall;
 import org.concordion.api.Element;
 import org.concordion.api.Evaluator;
 import org.concordion.api.ResultRecorder;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
@@ -20,7 +22,10 @@ import static org.mockserver.model.StringBody.exact;
 
 @Component
 public class RequestBodyTag extends MockServerTag {
-	private static final String MOCK_REQUEST_BODY_VARIABLE = "#requestBody";
+	@Autowired
+	private JsonPrettyPrinter jsonPrettyPrinter;
+	@Autowired
+	private XmlPrettyPrinter xmlPrettyPrinter;
 
 	public RequestBodyTag() {
 		setName("body");
@@ -30,29 +35,35 @@ public class RequestBodyTag extends MockServerTag {
 	public void setUp(CommandCall commandCall, Evaluator evaluator, ResultRecorder resultRecorder) {
 		Element element = commandCall.getElement();
 		String resource = element.getAttributeValue("resource");
-		String body = null;
+		String body = element.getText();
+
 		if (resource != null) {
 			try {
 				body = IOUtils.toString(Files.newInputStream(Paths.get(ClassLoader.getSystemResource(resource).toURI())));
+				element.appendText(body);
 			} catch (IOException | URISyntaxException e) {
 				e.printStackTrace();
 			}
-		} else {
-			body = new JsonPrettyPrinter().prettyPrint(element.getText());
 		}
-		element.appendText(body);
-		evaluator.setVariable(MOCK_REQUEST_BODY_VARIABLE, body);
-
 		RequestTag.requestFromEvaluator(evaluator).withBody(exact(body));
 	}
 
 	@Override
 	public void verify(CommandCall commandCall, Evaluator evaluator, ResultRecorder resultRecorder) {
 		Element element = commandCall.getElement();
+		String body = element.getText();
+		String classMarker = element.getAttributeValue("class");
+		if (classMarker.contains("xml")) {
+			body = xmlPrettyPrinter.prettyPrint(body);
+		}
+		if (classMarker.contains("json")) {
+			body = jsonPrettyPrinter.prettyPrint(body);
+			element.appendText(body);
+		}
 
-		String body = JsonWriter.formatJson((String) evaluator.getVariable(MOCK_REQUEST_BODY_VARIABLE));
 		element.moveChildrenTo(new Element("span"));
 		element.appendText(body);
+
 		super.verify(commandCall, evaluator, resultRecorder);
 	}
 
